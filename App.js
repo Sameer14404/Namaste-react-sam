989753a1bc86e5bba4d1642f45ae6513a710070a
sameer dangi <sameerdangi66@gmail.com>
2024-01-09 07:23:48 -1200

    done with episode-3

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+let parent=<h1 id="parent"> Episode-3</h1>
+
+let root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
--- README.md
+++
@@ -1,2 +0,0 @@
-# Namaste-react-sam
-All the depth knowledge and assigment of the react related stuff
