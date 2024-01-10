d23e5664c78c0b7550b1ad2fbb3e8cd14ce189f3
sameer dangi <sameerdangi66@gmail.com>
2024-01-10 10:15:50 -1200

    how to make components

--- App.js
+++ App.js
@@ -1,7 +1,49 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-let parent=<h1 id="parent"> Episode-3</h1>
+const Header=()=>{
+    return(
+        <div className="header-container">
+          
+            <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwlz25QIlCMRKVQialJlbT93VwxoopTDqlXQ&usqp=CAU" className="image"/>
+            </div>
+             
+               
+             <div className="nav-items">
+             <ul>Home</ul>
+                <ul>about</ul>
+                <ul>contact-us</ul>
+                <ul>cart</ul>
+             </div>
+        </div>
+       
+    )
+}
+
+const RestrauntCard=()=>{
+    return(
+
+        <div className="res-container">
+ <div className="res-card">
+        <img src="https://graphicsfamily.com/wp-content/uploads/edd/2023/02/Restaurant-Logo-Design-2-scaled.jpg" alt=""/>
+        <h4>Name</h4>
+        <h4>asian/south-indian</h4>
+        <h4>rating</h4>
+        <h4>Delivery time</h4>
+        
+    </div>
+        </div>
+       
+    )
+}
+
+const Applayout=()=>{
+   return(<div>
+    <Header/>
+    <RestrauntCard/>
+   </div>)
+}
 
 let root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Applayout/>);
